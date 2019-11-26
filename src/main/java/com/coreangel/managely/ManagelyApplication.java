f009d2a45ce803232056f2f37506b1851442a00a
Patryk Szafran <bragon912@gmail.com>
2019-11-26 23:39:00 +0100

    initial commit

---
+++ src/main/java/com/coreangel/managely/ManagelyApplication.java
@@ -0,0 +1,13 @@
+package com.coreangel.managely;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagelyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagelyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coreangel/managely/ManagelyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coreangel.managely;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagelyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
